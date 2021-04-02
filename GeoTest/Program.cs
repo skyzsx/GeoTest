1cccccca844cb98c42f3e6183632db9ea79027ec
skyzsx <skyzsx@vip.qq.com>
2021-04-02 23:24:51 +0800

    chuangjian

---
+++ GeoTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GeoTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
